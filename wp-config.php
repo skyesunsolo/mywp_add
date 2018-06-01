20f518002239465d0f454dd1e7dafcff131b6544
sky <skyesunsolo@gmail.com>
2018-06-01 10:52:29 +1000

    1

--- wp-config.php
+++ wp-config.php
@@ -18,18 +18,19 @@
  * @package WordPress
  */
 
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'skyesun');
 
 /** MySQL database username */
-define('DB_USER', 'localhost');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '799c94ffbd8ad83e');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'skyesun');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
