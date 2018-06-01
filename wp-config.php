4963a21e10859f65dea4d75127e185ff7ffc95ab
sky <skyesunsolo@gmail.com>
2018-06-01 10:38:01 +1000

    1

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'skyesun');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'skyesun');
 
 /** MySQL database password */
 define('DB_PASSWORD', '799c94ffbd8ad83e');
